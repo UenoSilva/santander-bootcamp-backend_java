e3b2867756accc7e4623f5209dbdf14027489bbf
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-09-17 12:40:06 -0300

    feat: interface video

---
+++ java_basico-uml_iphone/Iphone/src/com/dio/ueno/main/Interfaces/VideoInterface.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.dio.ueno.main.Interfaces;
+
+/**
+ *
+ * @author Ueno
+ */
+public interface VideoInterface {
+
+    void gravar();
+
+    void salvar();
+
+    void excluir();
+
+    void visualizarVideo(String nome);
+
+}
