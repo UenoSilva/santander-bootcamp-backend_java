a98e924e23baf5bc9b8236b13fd01aa23e329ec0
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-10-14 15:28:49 -0300

    feat: adicionando o repository ao projeto

---
+++ java_springboot-desafio/restfullapi-animes/src/main/java/com/dio/ueno/domain/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package com.dio.ueno.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dio.ueno.domain.model.Anime;
+
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+}
