697fe4dcfe8f03db1b94d00f41eba9c7aa569270
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-10-14 14:56:56 -0300

    feat: criando o projeto

---
+++ java_springboot-desafio/restfullapi-animes/src/main/java/com/dio/ueno/RestfullapiAnimesApplication.java
@@ -0,0 +1,13 @@
+package com.dio.ueno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullapiAnimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullapiAnimesApplication.class, args);
+	}
+
+}
---
+++ java_springboot-desafio/restfullapi-animes/src/test/java/com/dio/ueno/RestfullapiAnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.ueno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullapiAnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
